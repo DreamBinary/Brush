24990b081f03c2729d2e7dbc1d980f8abe1928db
cxq <1647715359@qq.com>
2023-07-23 20:49:47 +0800

    0

--- Brush Watch App/UI/Component/AnimViolin.swift
+++ Brush Watch App/UI/Component/AnimViolin.swift
@@ -11,8 +11,6 @@ struct AnimViolin: View {
     @State var dx: Double = 0
     @State var angle: Double = 0
 
-    @State var attempts: Int = 0
-
     var body: some View {
         GeometryReader { geo in
             let width = geo.size.width
@@ -24,7 +22,6 @@ struct AnimViolin: View {
                     .opacity(0.6)
                     .frame(width: width * 0.70)
                     .rotationEffect(.degrees(angle))
-//                    .modifier(Shake(animatableData: CGFloat(attempts)))
 
                 Image("Bow")
                     .resizable()  
@@ -34,34 +31,14 @@ struct AnimViolin: View {
                     .padding(.trailing, width * 0.3)
                     .padding(.top, height * 0.05)
                     .offset(x: dx, y: -dx * tan(75.0 / 180.0 * Double.pi))
-
-//                    .onTapGesture {
-//
-//                    }
-
             }.frame(maxWidth: .infinity, maxHeight: .infinity)
                 .onAppear {
                     withAnimation(.easeInOut(duration: 1).repeatForever(autoreverses: true)) {
                         dx = 5
-                    }
-                    withAnimation(.easeInOut(duration: 1).repeatForever(autoreverses: true)) {
                         angle = 2
                     }
                 }
         }.edgesIgnoringSafeArea(.all)
-    }
-}
-
-struct Shake: GeometryEffect {
-    var amount: CGFloat = 10
-    var shakesPerUnit = 3
-    var animatableData: CGFloat
-
-    func effectValue(size: CGSize) -> ProjectionTransform {
-        ProjectionTransform(
-            CGAffineTransform(translationX:
-                amount * sin(animatableData * .pi * CGFloat(shakesPerUnit)),
-                y: 0))
     }
 }
 
