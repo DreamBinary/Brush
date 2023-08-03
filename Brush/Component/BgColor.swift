175dc005ed74d1688779f54439d365f7f57c8ef6
cxq <1647715359@qq.com>
2023-08-03 16:05:31 +0800

    0

--- Brush/Component/BgColor.swift
+++ Brush/Component/BgColor.swift
@@ -12,24 +12,24 @@ struct BgColor<Content>: View where Content: View {
 
     var body: some View {
         content()
-            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            .frame(width: screenWidth, height: screenHeight)
             .background(
-              LinearGradient(
-                stops: [
-                  Gradient.Stop(color: Color(red: 0.49, green: 0.89, blue: 0.82), location: 0.05),
-                  Gradient.Stop(color: Color(red: 0.61, green: 0.92, blue: 0.86), location: 0.47),
-                  Gradient.Stop(color: Color(red: 0.09, green: 0.76, blue: 0.65), location: 1.00),
-                ],
-                startPoint: UnitPoint(x: 0.55, y: 0.02),
-                endPoint: UnitPoint(x: 0.98, y: 1)
-              )
+                LinearGradient(
+                    stops: [
+                        Gradient.Stop(color: Color(red: 0.49, green: 0.89, blue: 0.82), location: 0.05),
+                        Gradient.Stop(color: Color(red: 0.61, green: 0.92, blue: 0.86), location: 0.47),
+                        Gradient.Stop(color: Color(red: 0.09, green: 0.76, blue: 0.65), location: 1.00),
+                    ],
+                    startPoint: UnitPoint(x: 0.55, y: 0.02),
+                    endPoint: UnitPoint(x: 0.98, y: 1)
+                )
             )
     }
 }
 
 struct BgColor_Previews: PreviewProvider {
     static var previews: some View {
-        BgColor(){
+        BgColor {
             Spacer()
         }
     }
